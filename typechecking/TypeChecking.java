282295479a999a2fa9c44f5f7e1c1566f05e2910
Fernanda Bezerra <fbn.prg@gmail.com>
2019-04-18 20:08:17 -0300

    [+] It adds the typechecking of class

--- typechecking/TypeChecking.java
+++ typechecking/TypeChecking.java
@@ -54,8 +54,25 @@ public class TypeChecking implements TypeVisitor {
         for ( int i = 0; i < n.statementList.size(); i++ ) {
             pMethodDefinition.statementList.elementAt(i).visit(this);
         }
-        pMethodDefinition.expression.visit(this);
+        pMethodDefinition.expression.accept(this);
     }
+
+    public void visit(Class pClass) {
+        public VarDeclaration varDeclaration ;
+        public MethodDeclaration methodDeclaration;
+        VarDeclaration var = pClass.varDeclaration.accept(this);
+        VarDeclaration method = pClass.methodDeclaration.accept(this);
+        String id1 = pClass.identifier1.toString();
+        String id2 = pClass.identifier2.toString();
+        //TODO I dont know how we going to do in the table to add classes
+        if (!programTable.addVar(id1)) {
+            error.complain(id + "is already defined in " + programTable.getId());
+        }
+        if (!programTable.addVar(id2)) {
+            error.complain(id + "is already defined in " + programTable.getId());
+        }
+    }
+
     public Type visit(PlusExpression pPlus) {
         if (! (pPlus.e1.accept(this) instanceof IntegerType) )
             error.complain("Left side of LessThan must be of type integer");
