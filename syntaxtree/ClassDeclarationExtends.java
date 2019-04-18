d5615074eefd688af56823ce3f1dbff94f46b2ef
Fernanda Bezerra <fbn.prg@gmail.com>
2019-04-18 09:31:52 -0300

    [+] It adds the class

---
+++ syntaxtree/ClassDeclarationExtends.java
@@ -0,0 +1,23 @@
+package syntaxtree;
+import visitor.Visitor;
+import visitor.TypeVisitor;
+
+public class ClassDeclExtends extends Class {
+    public Identifier i;
+    public Identifier j;
+    public VarDefinitionList vl;
+    public MethodDefinitionList ml;
+
+    public ClassDeclExtends(Identifier ai, Identifier aj,
+                            VarDefinitionList avl, MethodDefinitionList aml) {
+        i=ai; j=aj; vl=avl; ml=aml;
+    }
+
+    public void accept(Visitor v) {
+        v.visit(this);
+    }
+
+    public Type accept(TypeVisitor v) {
+        return v.visit(this);
+    }
+}
