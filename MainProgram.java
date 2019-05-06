4937b2fb591e19da318021138472ff64a05ffc12
AndyFernandes <andfernandes24@gmail.com>
2019-05-06 14:34:12 -0300

    [+] tentando remover pasta lixo

--- MainProgram.java
+++ MainProgram.java
@@ -7,21 +7,20 @@ import analisador_semantico.visitors.TypeChecking;
 import java.io.*;
 
 public class MainProgram {
-	public static void main(String[] args) throws Exception
-	{		
-		//String filePath = new File("").getAbsolutePath();
-		//System.out.println(filePath);
-		//Parser parser = new Parser(new FileReader(filePath+path));
+	public static void main(String[] args) throws Exception {
+		// String filePath = new File("").getAbsolutePath();
+		// System.out.println(filePath);
+		// Parser parser = new Parser(new FileReader(filePath+path));
 		Parser parser = new Parser(new java.io.FileInputStream(args[0]));
 		Program programa = parser.Goal();
-		
-        //Declarar o visitor pra construção da tabela de símbolos e chamar abaixo
-        SymbolTable table = new SymbolTable();
-        programa.accept(table);
 
-		///--------------------------------------
+		// Declarar o visitor pra construção da tabela de símbolos e chamar abaixo
+		SymbolTable table = new SymbolTable();
+		programa.accept(table);
+
+		/// --------------------------------------
 		// Declarar o visitor pra checagem de tipos e chamar abaixo
 		TypeChecking checking = new TypeChecking(table.getTable());
-        programa.accept(checking);
+		programa.accept(checking);
 	}
 }
