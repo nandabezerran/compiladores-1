aa1087e2c485793d0b2b41b3ab6565ca41f3cd93
AndyFernandes <andfernandes24@gmail.com>
2019-05-08 14:44:45 -0300

    [+] Ajeitando o MainProgram

--- MainProgram.java
+++ MainProgram.java
@@ -1,9 +1,12 @@
 import analisador_lexico.*;
 import analisador_semantico.syntaxtree.*;
 import analisador_semantico.syntaxtree.Program;
+import analisador_semantico.visitors.ImplIR;
 import analisador_semantico.visitors.SymbolTable;
 import analisador_semantico.visitors.TypeChecking;
-
+import traducao_intermediario.visitor.*;
+import traducao_intermediario.Translate.Exp;
+import traducao_intermediario.Mips.*;
 import java.io.*;
 
 public class MainProgram {
@@ -22,5 +25,10 @@ public class MainProgram {
 		// Declarar o visitor pra checagem de tipos e chamar abaixo
 		TypeChecking checking = new TypeChecking(table.getTable());
 		programa.accept(checking);
+
+		// --------------------------------------
+		// Declarar o visitor pra tradução do código intermediário
+		ImplIR intermediario = new ImplIR(table.getTable(), new MipsFrame()); // TODO passar os parametros do MipsFrame
+		programa.accept(intermediario);
 	}
 }
